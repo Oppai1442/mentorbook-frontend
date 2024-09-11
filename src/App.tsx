3f26693270f91ae8bb4ca25652c24a823f49bb5a
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-11 19:17:22 +0700

    Update App to use router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
+// App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import routes from './routes';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Trang Chủ</Link>
+          </li>
+          <li>
+            <Link to="/about">Giới Thiệu</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={React.createElement(route.element)} />
+        ))}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
