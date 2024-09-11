041ee1c4b46f8547e2758dbe10aa64ae0674e6f2
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-11 19:17:08 +0700

    Create new 3 components

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+// About.tsx
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h2>Giới Thiệu</h2>
+      <p>Đây là trang giới thiệu của ứng dụng.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+// Home.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h2>Giới Thiệu</h2>
+      <p>Đây là trang giới thiệu của ứng dụng.</p>
+    </div>
+  );
+};
+
+export default Home;
