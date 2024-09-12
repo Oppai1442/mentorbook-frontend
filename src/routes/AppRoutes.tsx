f7edb2db115c6c17c5ef52029ece20f46d86af35
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-13 03:02:08 +0700

    Fix route path

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,7 @@
 // routes.ts
-import About from '../components/About';
-import Home from '../components/Home';
-import NotFound from '../components/NotFound';
+import { About } from '../pages/About';
+import { Home } from '../pages/Home';
+import { NotFound } from '../pages/NotFound';
 
 const routes = [
   { path: '/', element: Home },
