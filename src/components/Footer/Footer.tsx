ff4f7a3f37479a5c58ed1107068a567734e6d869
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-13 03:03:11 +0700

    Modify Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,1 +1,24 @@
-export {};
+// src/components/Footer/Footer.tsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.module.css'; // Giả sử bạn có file CSS cho Footer
+
+const Footer: React.FC = () => {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-list">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Footer;
