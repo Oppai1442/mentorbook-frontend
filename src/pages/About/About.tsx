849ee7dddbd23dfc2c4a186e85b27eb02bc03554
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-13 03:02:23 +0700

    Modify About page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,1 +1,13 @@
-export {};
+// src/pages/Home/Home.tsx
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page!</h1>
+            {/* Có thể thêm các component khác ở đây */}
+        </div>
+    );
+};
+
+export default About;
