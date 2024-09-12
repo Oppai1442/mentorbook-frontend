58a3509bd4fc6ae5a07a6696683d9d9fe2891f38
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-12 20:44:27 +0700

    Remove old router, create new one in Routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,1 +1,12 @@
-export {};
+// routes.ts
+import About from '../components/About';
+import Home from '../components/Home';
+import NotFound from '../components/NotFound';
+
+const routes = [
+  { path: '/', element: Home },
+  { path: '/about', element: About },
+  { path: '*', element: NotFound },
+];
+
+export default routes;
