a4bb90cfbf9f9a4cf58b80dcc96d052effbb7554
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-18 07:30:49 +0700

    Create about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,12 @@
 // src/pages/Home/Home.tsx
 import React from 'react';
+import './About.css';
 
 const About: React.FC = () => {
     return (
         <div>
-            <h1>Welcome to the Home Page!</h1>
+            <h1>Welcome about!</h1>
+            <h1 className='viao729-sRtg'>Test message</h1>
             {/* Có thể thêm các component khác ở đây */}
         </div>
     );
