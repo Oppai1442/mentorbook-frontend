b81f58ffd7059c3d6cb760a361cea6f9a4af66c9
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-18 07:27:59 +0700

    Create config file, allow toggle components in page

---
+++ src/config/pageConfig.ts
@@ -0,0 +1,13 @@
+// src/pageConfig.ts
+interface PageConfig {
+    showNav: boolean;
+    showFooter: boolean;
+    showContact: boolean;
+  }
+  
+  export const pageConfigs: { [key: string]: PageConfig } = {
+    home: { showNav: true, showFooter: true, showContact: true },
+    about: { showNav: true, showFooter: true, showContact: false },
+    notFound: { showNav: false, showFooter: false, showContact: false },
+  };
+  
