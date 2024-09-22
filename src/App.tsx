14fdf838e7e8ef35076c9b18df0b8a3b4db5f8c7
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-22 11:27:35 +0700

    Import global css file

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import routes from './routes';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { pageConfigs } from './config/pageConfig';
+import './styles/global.css'
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const location = useLocation();
-  const path = location.pathname.substring(1) || 'home'; // Lấy tên route (mặc định là 'home' nếu route là '/')
+  const path = location.pathname.substring(1) || 'home';
   const config = pageConfigs[path] || pageConfigs['notFound']; // Dùng cấu hình tương ứng cho từng page
 
   return (
