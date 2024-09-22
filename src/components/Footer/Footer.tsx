44324f39ca0e59cd5e37d406da0fe913c779af0b
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-22 11:30:50 +0700

    Update footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,274 +4,85 @@ import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="footer footer-one footer-five">
-  <div className="footer-top aos aos-init aos-animate" data-aos="fade-up">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-2 col-md-4">
-          <div className="footer-widget footer-menu">
-            <h2 className="footer-title">Company</h2>
-            <ul>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/index-5.html">
-                  What we offer
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/index-5.html">
-                  Careers
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/index-5.html">
-                  Press
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/index-5.html">
-                  Become a Mentor
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/index-5.html">
-                  Affilates
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-lg-2 col-md-4">
-          <div className="footer-widget footer-menu">
-            <h2 className="footer-title">Community</h2>
-            <ul>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/search.html">
-                  Learners
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/login.html">
-                  Partners
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/register.html">
-                  Developers
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/booking.html">
-                  Free Courses
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/dashboard-mentee.html">
-                  Affilates
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/dashboard-mentee.html">
-                  Blogs
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/dashboard-mentee.html">
-                  Become a Mentee
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-lg-2 col-md-4">
-          <div className="footer-widget footer-menu">
-            <h2 className="footer-title">More</h2>
-            <ul>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/appointments.html">
-                  Appointments
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/chat.html">
-                  Chat
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/login.html">
-                  Login
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/register.html">
-                  Register
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/dashboard.html">
-                  Dashboard
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/booking.html">
-                  Booking
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/search.html">
-                  Search Mentors
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-lg-2 col-md-4">
-          <div className="footer-widget footer-menu">
-            <h2 className="footer-title">Quick Links</h2>
-            <ul>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/appointments.html">
-                  Terms &amp; Conditions
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/chat.html">
-                  Privacy Policy
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/login.html">
-                  Contact us
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/register.html">
-                  Register
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/dashboard.html">
-                  Sitemap
-                </a>
-              </li>
-              <li>
-                <a href="https://mentoring.dreamstechnologies.com/html/template/dashboard.html">
-                  Featured
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6 ">
-          <div className="footer-widget footer-subscribe bg-subscribe">
-            <h2 className="footer-title">Subscribe</h2>
-            <div className="footer-mail">
-              <form action="https://mentoring.dreamstechnologies.com/html/template/search.html">
-                <div className="form-group">
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Your email Addess"
-                  />
-                </div>
-                <div className="input-group">
-                  <a
-                    href="https://mentoring.dreamstechnologies.com/html/template/index-5.html"
-                    className="btn btn-primary w-100"
-                  >
-                    Subscribe
-                  </a>
-                </div>
-              </form>
-            </div>
-            <p className="subscribe-text">
-              Our goal is to translate the positive effects from revolutionizing
-              how companies engage with their clients &amp; their team.
+      <footer className="bg-dark text-light py-4">
+      <div className="container">
+        <div className="row">
+          {/* About Section */}
+          <div className="col-md-4 col-12">
+            <h5>About Us</h5>
+            <p>
+              We provide an easy and reliable way to connect with mentors for
+              professional guidance and development.
             </p>
-            <ul className="social-icon">
+          </div>
+          {/* Quick Links Section */}
+          <div className="col-md-4 col-12">
+            <h5>Quick Links</h5>
+            <ul className="list-unstyled">
               <li>
-                <a
-                  href="https://mentoring.dreamstechnologies.com/html/template/index-5.html#"
-                  className="social-icon-space d-flex align-items-center justify-content-center"
-                  target="_blank"
-                >
-                  <i className="fab fa-facebook-f" />
+                <a href="/" className="text-light">
+                  Home
                 </a>
               </li>
               <li>
-                <a
-                  href="https://mentoring.dreamstechnologies.com/html/template/index-5.html#"
-                  className="social-icon-space d-flex align-items-center justify-content-center"
-                  target="_blank"
-                >
-                  <i className="fab fa-instagram" />
+                <a href="/about" className="text-light">
+                  About
                 </a>
               </li>
               <li>
-                <a
-                  href="https://mentoring.dreamstechnologies.com/html/template/index-5.html#"
-                  className="social-icon-space d-flex align-items-center justify-content-center"
-                  target="_blank"
-                >
-                  <i className="fab fa-linkedin" />
+                <a href="/contact" className="text-light">
+                  Contact Us
                 </a>
               </li>
               <li>
-                <a
-                  href="https://mentoring.dreamstechnologies.com/html/template/index-5.html#"
-                  className="social-icon-space d-flex align-items-center justify-content-center"
-                  target="_blank"
-                >
-                  <i className="fab fa-twitter" />
+                <a href="/faq" className="text-light">
+                  FAQ
                 </a>
               </li>
             </ul>
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div className="footer-bottom ">
-    <div className="container">
-      <div className="copyright-border" />
-      <div className="copyright ">
-        <div className="row align-items-center">
-          <div className="col-lg-6">
-            <div className="copyright-text">
-              <p className="mb-0">© 2023 Mentoring. All rights reserved.</p>
-            </div>
+          {/* Contact Information Section */}
+          <div className="col-md-4 col-12">
+            <h5>Contact Us</h5>
+            <p>
+              <i className="fas fa-envelope" /> support@mentorbooking.com
+            </p>
+            <p>
+              <i className="fas fa-phone" /> +123 456 7890
+            </p>
+            <p>
+              <i className="fas fa-map-marker-alt" /> 123 Mentor St, Education City,
+              USA
+            </p>
           </div>
-          <div className="col-lg-6">
-            <div className="term-privacy">
-              <div className="bottom-links">
-                <ul>
-                  <li>
-                    <a href="javascript:void(0);">About</a>
-                  </li>
-                  <li>
-                    <a href="javascript:void(0);">Become a Mentor</a>
-                  </li>
-                  <li>
-                    <a href="javascript:void(0);">Become a Mentee</a>
-                  </li>
-                  <li>
-                    <a href="javascript:void(0);">Pricing</a>
-                  </li>
-                  <li>
-                    <a href="javascript:void(0);">All Courses</a>
-                  </li>
-                  <li>
-                    <a href="javascript:void(0);">Help</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+        </div>
+        {/* Social Media Links */}
+        <div className="row mt-3">
+          <div className="col text-center">
+            <a href="#" className="text-light mx-2">
+              <i className="fab fa-facebook" />
+            </a>
+            <a href="#" className="text-light mx-2">
+              <i className="fab fa-twitter" />
+            </a>
+            <a href="#" className="text-light mx-2">
+              <i className="fab fa-linkedin" />
+            </a>
+            <a href="#" className="text-light mx-2">
+              <i className="fab fa-instagram" />
+            </a>
+          </div>
+        </div>
+        {/* Copyright Section */}
+        <div className="row mt-3">
+          <div className="col text-center">
+            <p className="mb-0">© 2024 Mentor Booking. All rights reserved.</p>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</footer>
-
+    </footer>
+    
+    
     );
 };
 
