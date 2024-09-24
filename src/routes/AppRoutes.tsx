9b9b8024c4fedce7e49b11ac797cd92a2fa31dda
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-24 14:10:36 +0700

    Update route: add new route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,18 @@
 import { About } from '../pages/About';
 import { Home } from '../pages/Home';
 import { Auth } from '../pages/Auth';
+import { Contact } from '../pages/Contact';
+import { MentorList } from '../pages/MentorList';
+import { FAQs } from '../pages/FAQs';
 import { NotFound } from '../pages/NotFound';
 
 const routes = [
   { path: '/', element: Home },
   { path: '/about', element: About },
-  { path: '/auth', element: Auth},
+  { path: '/auth', element: Auth },
+  { path: '/contact', element: Contact },
+  { path: '/mentors', element: MentorList },
+  { path: '/FAQs', element: FAQs },
   { path: '*', element: NotFound },
 ];
 
