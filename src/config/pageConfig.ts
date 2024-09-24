d617e65bf3380070779533ca4931ea608167372a
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-24 14:09:56 +0700

    Update config: add new config for page

--- src/config/pageConfig.ts
+++ src/config/pageConfig.ts
@@ -1,3 +1,5 @@
+import { mentors } from "../pages/MentorList/mockData";
+
 // src/pageConfig.ts
 interface PageConfig {
     showNav: boolean;
@@ -8,6 +10,10 @@ interface PageConfig {
   export const pageConfigs: { [key: string]: PageConfig } = {
     home: { showNav: true, showFooter: true, showContact: false },
     about: { showNav: true, showFooter: true, showContact: false },
+    contact: { showNav: true, showFooter: true, showContact: false },
+    mentors: { showNav: true, showFooter: true, showContact: false },
+    auth: { showNav: true, showFooter: true, showContact: false },
+    FAQs: { showNav: true, showFooter: true, showContact: false },
     notFound: { showNav: false, showFooter: false, showContact: false },
   };
   
