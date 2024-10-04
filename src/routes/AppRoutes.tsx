ba9bd7cea0519bcecca329dca5f16ed9d5d13a40
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-04 18:32:23 +0700

    Update Routes: Remove /auth Path and Auth from Routing

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,6 @@
 // routes.ts
 import { About } from '../pages/About';
 import { Home } from '../pages/Home';
-import { Auth } from '../pages/Auth';
 import { Contact } from '../pages/Contact';
 import { MentorList } from '../pages/MentorList';
 import { FAQs } from '../pages/FAQs';
@@ -13,7 +12,6 @@ import { NotFound } from '../pages/NotFound';
 const routes = [
   { path: '/', element: Home },
   { path: '/about', element: About },
-  { path: '/auth', element: Auth },
   { path: '/contact', element: Contact },
   { path: '/mentors', element: MentorList },
   { path: '/FAQs', element: FAQs },
