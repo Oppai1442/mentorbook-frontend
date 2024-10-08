b5652e80b37e22a402313fa12196e945a251b30d
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-08 20:40:18 +0700

    Update Config: Add Configuration for showChatBubble and showCookie

--- src/config/pageConfig.ts
+++ src/config/pageConfig.ts
@@ -1,18 +1,19 @@
 interface PageConfig {
     showNav: boolean;
     showFooter: boolean;
-    showContact: boolean;
+    showChatBubble: boolean;
+    showCookie: boolean;
   }
   
   export const pageConfigs: { [key: string]: PageConfig } = {
-    about: { showNav: true, showFooter: true, showContact: false },
-    auth: { showNav: true, showFooter: true, showContact: false },
-    contact: { showNav: true, showFooter: true, showContact: false },
-    FAQs: { showNav: true, showFooter: true, showContact: false },
-    home: { showNav: true, showFooter: true, showContact: false },
-    mentors: { showNav: true, showFooter: true, showContact: false },
-    Policy: { showNav: true, showFooter: true, showContact: false },
-    ToS: { showNav: true, showFooter: true, showContact: false },
-    notFound: { showNav: false, showFooter: false, showContact: false },
+    about: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
+    auth: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
+    contact: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
+    FAQs: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
+    home: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
+    mentors: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
+    Policy: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
+    ToS: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
+    notFound: { showNav: false, showFooter: false, showChatBubble: false, showCookie: false},
   };
   
