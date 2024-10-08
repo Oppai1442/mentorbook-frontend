6b69a6d32c44e20bbe7f2ab9ab84d7fa0be18765
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-08 20:22:59 +0700

    Create Cookie Component: Add Functionality and Styles

---
+++ src/components/Cookie/Cookie.tsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Cookie.module.css";
+
+const CookieConsent: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isAccept, setIsAccept] = useState(() => {
+    const saved = localStorage.getItem('acceptCookie');
+    return saved === 'true';
+  });
+
+  useEffect(() => {
+    const saved = localStorage.getItem('acceptCookie');
+    if (saved === 'true' || saved === 'false') {
+      setIsVisible(false);
+    } else {
+        setIsVisible(true);
+    }
+  }, [isAccept]);
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
+  const handleAcceptAll = () => {
+    setIsAccept(true);
+    localStorage.setItem('acceptCookie', isAccept.toString());
+    handleClose();
+  };
+
+  const handleDeclineAll = () => {
+    setIsAccept(false);
+    localStorage.setItem('acceptCookie', isAccept.toString());
+    handleClose();
+  };
+
+  return (
+    <>
+      {isVisible && (
+        <section
+          className={`${styles["position-fixed"]} ${styles["bottom-0"]} ${styles["start-0"]} ${styles["p-4"]}`}
+        >
+          <div
+            className={`${styles["position-relative"]} ${styles["mw-md"]} ${styles["py-4"]} ${styles["ps-4"]} ${styles["pe-8"]} ${styles["bg-black"]} ${styles["rounded-5"]}`}
+          >
+            <button
+              type="button"
+              onClick={handleClose}
+              className={`${styles["position-absolute"]} ${styles["top-0"]} ${styles["end-0"]} ${styles["me-6"]} ${styles["mt-6"]} ${styles["btn"]} ${styles["p-0"]}`}
+            >
+              <i className="fa-thin fa-x"></i>
+            </button>
+            <div className={`${styles["container"]} ${styles["position-relative"]}`}>
+              <div className={`${styles["row"]} ${styles["align-items-center"]}`}>
+                <h3 className={`${styles["mb-2"]} ${styles["fs-6"]} ${styles["fw-medium"]}`}>
+                  Cookie Consent
+                </h3>
+                <p className={`${styles["text-light"]} ${styles["mb-8"]}`}>
+                  We use cookies to ensure that we give you the best experience on
+                  our website. If you continue to use this site we will assume that
+                  you are happy with it.
+                </p>
+                <div className={`${styles["d-flex"]} ${styles["flex-column"]} ${styles["flex-sm-row"]} ${styles["gap-4"]}`}>
+                  <a
+                    className={`${styles["btn"]} ${styles["btn-primary"]} ${styles["fw-medium"]}`}
+                    onClick={handleAcceptAll}
+                  >
+                    Allow
+                  </a>
+                  <a
+                    className={`${styles["btn"]} ${styles["btn-outline-light"]} ${styles["fw-medium"]}`}
+                    onClick={handleDeclineAll}
+                  >
+                    Decline
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
+  );
+};
+
+export default CookieConsent;
