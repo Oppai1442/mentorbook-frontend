5235e3317a78a0396716ea6308b24e051e7c5012
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-20 15:53:02 +0700

    Updated: Changed from default import to named import

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-route
 import { pageConfigs } from './config/pageConfig';
 import { ToastProvider } from './context/ToastContext';
 
-import routes from './routes';
-import Navbar from './components/Navbar';
-import ChatBubble from './components/ChatBubble';
-import Footer from './components/Footer';
-import CookieConsent from './components/Cookie';
+import { routes } from './routes';
+import { Navbar } from './components/Navbar';
+import { ChatBubble } from './components/ChatBubble';
+import { Footer } from './components/Footer';
+import { CookieConsent } from './components/Cookie';
 
 import styles from './styles/App.module.css';
 import './styles/global.css'
@@ -23,13 +23,13 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <>
       <ToastProvider>
-      <AuthProvider>
+        <AuthProvider>
           {config.showNav && <Navbar />}
           <main className={`${styles['mainContainer']}`}>{children}</main>
           {config.showCookie && <CookieConsent />}
           {config.showChatBubble && <ChatBubble />}
           {config.showFooter && <Footer />}
-      </AuthProvider>
+        </AuthProvider>
       </ToastProvider>
     </>
   );
--- src/config/pageConfig.ts
+++ src/config/pageConfig.ts
@@ -1,19 +1,18 @@
 interface PageConfig {
-    showNav: boolean;
-    showFooter: boolean;
-    showChatBubble: boolean;
-    showCookie: boolean;
-  }
-  
-  export const pageConfigs: { [key: string]: PageConfig } = {
-    about: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
-    auth: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
-    contact: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
-    FAQs: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
-    home: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
-    mentors: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
-    Policy: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
-    ToS: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true},
-    notFound: { showNav: false, showFooter: false, showChatBubble: false, showCookie: false},
-  };
-  
+  showNav: boolean;
+  showFooter: boolean;
+  showChatBubble: boolean;
+  showCookie: boolean;
+}
+
+export const pageConfigs: { [key: string]: PageConfig } = {
+  about: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true },
+  auth: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true },
+  contact: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true },
+  FAQs: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true },
+  home: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true },
+  mentors: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true },
+  Policy: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true },
+  ToS: { showNav: true, showFooter: true, showChatBubble: true, showCookie: true },
+  notFound: { showNav: false, showFooter: false, showChatBubble: false, showCookie: false },
+};
