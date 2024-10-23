fd6df45bfc7656d8c272e0affae31900f7d6c63c
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-23 07:55:50 +0700

    Updated Navbar: Updated CSS for dropdown menu and dropdown items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,22 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import { Link } from "react-router-dom";
-import styles from "./Navbar.module.css";
 import { loadSvgs } from "../../utils";
 import { Auth } from "../Auth";
 import { useAuth } from "../../context";
+import styles from "./Navbar.module.css";
 
 const Navbar: React.FC = () => {
   const [svgData, setSvgData] = useState<{ [key: string]: string | null }>({});
-  const { isLoggedIn, user, logOut } = useAuth(); 
-  const dropdownRef = useRef<HTMLUListElement | null>(null);
+  const { isLoggedIn, user, logOut } = useAuth();
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
 
   const [showAuth, setShowAuth] = useState(false);
-  const [authMode, setAuthMode] = useState<'signIn' | 'signUp' | null>(null);
+  const [authMode, setAuthMode] = useState<"signIn" | "signUp" | null>(null);
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const svgPaths = {
-      websiteLogo: () => import('../../assets/svg/website-logo.svg'),
+      websiteLogo: () => import("../../assets/svg/website-logo.svg"),
     };
 
     const loadAndSetSvgs = async () => {
@@ -27,10 +27,9 @@ const Navbar: React.FC = () => {
     loadAndSetSvgs();
   }, []);
 
-
   const handleClickOutside = useCallback((event: MouseEvent) => {
     const target = event.target as Node;
-    
+
     if (dropdownRef.current && !dropdownRef.current.contains(target)) {
       setIsOpen(false);
     }
@@ -52,14 +51,13 @@ const Navbar: React.FC = () => {
     signup: "/auth",
   };
 
-
   const handleSignIn = () => {
-    setAuthMode('signIn');
+    setAuthMode("signIn");
     setShowAuth(true);
   };
 
   const handleSignUp = () => {
-    setAuthMode('signUp');
+    setAuthMode("signUp");
     setShowAuth(true);
   };
 
@@ -69,15 +67,14 @@ const Navbar: React.FC = () => {
 
   const handleItemClick = (action?: string) => {
     switch (action) {
-      case 'logout':
-          logOut();
-          break;
+      case "logout":
+        logOut();
+        break;
       default:
         break;
     }
     setIsOpen(false);
   };
-
 
   return (
     <>
@@ -89,10 +86,10 @@ const Navbar: React.FC = () => {
             className={`${styles["a"]} ${styles["navbar-brand"]}`}
             to={pathList["home"]}
           >
-            {svgData['websiteLogo'] && (
+            {svgData["websiteLogo"] && (
               <img
                 className={`${styles["img-fluid"]} ${styles["img"]}`}
-                src={svgData['websiteLogo']}
+                src={svgData["websiteLogo"]}
                 alt=""
               />
             )}
@@ -140,42 +137,66 @@ const Navbar: React.FC = () => {
             </ul>
             <div className={`${styles["ms-14"]}`}>
               {isLoggedIn ? (
-                <div className={`${styles["d-flex"]} ${styles["align-items-center"]}`}>
+                <div
+                  className={`${styles["d-flex"]} ${styles["align-items-center"]}`}
+                >
                   <img
                     src={user?.avatarUrl}
                     alt="Avatar"
                     className={`${styles["rounded-circle"]} ${styles["me-2"]}`}
-                    style={{ width: '40px', height: '40px' }}
+                    style={{ width: "40px", height: "40px" }}
                   />
-                  <span className={`${styles["me-2"]} ${styles["username"]}`}>{user?.fullName}</span>
-                  <div className={`${styles["dropdown"]}`}>
+                  <span className={`${styles["me-2"]} ${styles["username"]}`}>
+                    {user?.fullName}
+                  </span>
+                  <div ref={dropdownRef} className={`${styles["dropdown"]}`}>
                     <button
                       className={`${styles["btn"]} ${styles["btn-secondary"]}`}
                       type="button"
                       onClick={toggleDropdown}
                       aria-expanded={isOpen}
                     >
-                      {!isOpen ? (<i className={`fa-light fa-caret-down ${styles["dropdown-arrow"]}`}></i>) :  (<i className={`fa-light fa-caret-up ${styles["dropdown-arrow"]}`}></i>)}
+                      <i
+                        className={`fa-light fa-caret-down ${styles["dropdown-arrow"]
+                          } ${isOpen ? styles["rotate"] : ""}`}
+                      ></i>
                     </button>
-                    {isOpen && (<ul ref={dropdownRef} className={`${styles["dropdown-menu"]}`}>
-
-                      <li>
-                        <Link className={`${styles["dropdown-item"]}`} to="/user/profile" onClick={() => handleItemClick()}>
-
-                          Log out
-                        </Link>
-                      </li>
-                      <li>
-                        <Link className={`${styles["dropdown-item"]}`} to="/user/setting" onClick={() => handleItemClick()}>
-                          Log out
-                        </Link>
-                      </li>
-                      <li>
-                        <button className={`${styles["dropdown-item"]}`} type="button" onClick={() => handleItemClick("logout")}>
-                          Log out
-                        </button>
-                      </li>
-                    </ul>)}
+                    {isOpen && (
+                      <ul
+                        className={`${styles["dropdown-menu"]}`}
+                      >
+                        <li>
+                          <Link
+                            className={`${styles["dropdown-item"]}`}
+                            to="/user/profile"
+                            onClick={() => handleItemClick()}
+                          >
+                            <i className="fa-light fa-user" />
+                            Profile
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            className={`${styles["dropdown-item"]}`}
+                            to="/user/setting"
+                            onClick={() => handleItemClick()}
+                          >
+                            <i className="fa-light fa-gear" />
+                            Settings
+                          </Link>
+                        </li>
+                        <li>
+                          <button
+                            className={`${styles["dropdown-item"]}`}
+                            type="button"
+                            onClick={() => handleItemClick("logout")}
+                          >
+                            <i className="fa-light fa-right-from-bracket" />
+                            Log out
+                          </button>
+                        </li>
+                      </ul>
+                    )}
                   </div>
                 </div>
               ) : (
