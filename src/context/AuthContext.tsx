4d70c7943d50995342e52edd17cf04a202c1d437
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-23 19:27:18 +0700

    Updated AuthContext: Moved login/register popup handling for reusability using useAuth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,18 +1,23 @@
 import React, { createContext, useState, ReactNode, useContext, useEffect } from "react";
 import { postData } from "../services/apiService";
 import { LoginResponse, User } from "../types";
+import { Auth } from "../components/Auth";
 
 interface AuthContextType {
   user: User | null;
   isLoggedIn: boolean;
   setUser: (user: User | null) => void;
   logOut: () => void;
+  renderAuth: () => JSX.Element | null;
+  showAuthModal: (mode: "signIn" | "signUp") => void;
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
+  const [showAuth, setShowAuth] = useState(false);
+  const [authMode, setAuthMode] = useState<"signIn" | "signUp" | null>(null);
 
   const isLoggedIn = !!user;
 
@@ -20,11 +25,10 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     const fetchUser = async () => {
       const accountToken = localStorage.getItem('accountToken');
       if (accountToken) {
-        const response = await postData<LoginResponse>("/user/login-token", {token: accountToken});
-        
-        if (response.isSuccess && response.data)  {
+        const response = await postData<LoginResponse>("/user/login-token", { token: accountToken });
+
+        if (response.isSuccess && response.data) {
           const { token, user } = response.data;
-          
           setUser(user);
           localStorage.setItem('accountToken', token);
         }
@@ -39,8 +43,19 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     setUser(null);
   };
 
+  const showAuthModal = (mode: "signIn" | "signUp") => {
+    setAuthMode(mode);
+    setShowAuth(true);
+  };
+
+  const renderAuth = () => {
+    if (!showAuth) return null;
+
+    return <Auth mode={authMode} onClose={() => setShowAuth(false)} />;
+  };
+
   return (
-    <AuthContext.Provider value={{ user, isLoggedIn, setUser, logOut}}>
+    <AuthContext.Provider value={{ user, isLoggedIn, setUser, logOut, renderAuth, showAuthModal }}>
       {children}
     </AuthContext.Provider>
   );
@@ -56,4 +71,4 @@ const useAuth = () => {
   return context;
 };
 
-export {useAuth}
+export { useAuth };
