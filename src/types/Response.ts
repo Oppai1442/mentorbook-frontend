8c03b52b354f841e90d718926000f091d9350b52
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-29 04:39:40 +0700

    Updated Response: Added SignupResponse, SigninResponse, and SkillResponse classes for handling respective response data

--- src/types/Response.ts
+++ src/types/Response.ts
@@ -1,6 +1,27 @@
-import { User } from "./index";
+import { User } from "./User";
 
-export default interface LoginResponse {
+export interface LoginResponse {
     token: string; 
     user: User;
+}
+
+export interface SignUpResponse {
+    token: string; 
+    user: User;
+}
+
+export interface mentorResponse {
+    userId: number;
+    fullName: string;
+    email: string;
+    avatarUrl: string;
+    backgroundUrl: string;
+    skills: SkillResponse[]
+}
+
+export interface SkillResponse {
+    length: number;
+    skillId: number;
+    skillName: string;
+    skillDescription: string;
 }
