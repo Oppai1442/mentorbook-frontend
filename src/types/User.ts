2639d608ff6bc3e02819a2cd378dd45d77c319d0
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-29 04:40:40 +0700

    Updated User: Added two interfaces for MentorData and Skill

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,7 +1,23 @@
-export default interface User {
+export interface User {
     id: number;
     fullName: string;
     email: string;
     avatarUrl: string;
     role: string;
+}
+
+export interface mentor {
+    userId: number;
+    fullName: string;
+    email: string;
+    avatarUrl: string;
+    backgroundUrl: string;
+    skills: skill[]
+}
+
+export interface skill {
+    length: number;
+    skillId: number;
+    skillName: string;
+    skillDescription: string;
 }
