6a99f15915a613cdda33b1b51bfa292cd7b25cb7
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-29 04:47:40 +0700

    Updated AuthContext: Changed import of user from ./types to ./types/user

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, ReactNode, useContext, useEffect } from "react";
-import { User } from "../types";
 import { Auth } from "../components/Auth";
 import { fetchUserFromToken } from "../services";
+import { User } from "../types/User";
 
 interface AuthContextType {
   user: User | null;
