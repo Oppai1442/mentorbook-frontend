11c342d21d04923d39136ff8923593dc63b05adc
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-29 04:52:54 +0700

    Updated index.ts in service: Adjusted exports for improved module organization

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,4 @@
 export { default as getGeocode } from './mapService';
 export { fetchUserFromToken, signIn, signUp } from './authService';
+export { getAllSkills } from './skillService';
+export { getAllMentors } from './mentorService';
