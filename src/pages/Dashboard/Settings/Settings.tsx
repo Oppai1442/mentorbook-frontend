6241aa26ae307205006da3e358a5079079dd303d
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-31 21:29:07 +0700

    Created Settings page: Added child page under Dashboard

---
+++ src/pages/Dashboard/Settings/Settings.tsx
@@ -0,0 +1,11 @@
+// Settings.tsx
+import React from 'react';
+
+const Settings: React.FC = () => (
+  <div>
+    <h2>Settings</h2>
+    <p>Adjust your settings here in the Settings section.</p>
+  </div>
+);
+
+export default Settings;
