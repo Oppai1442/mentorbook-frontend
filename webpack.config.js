d2894a02f21369566ed514758e2a995362836fd3
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-31 21:19:58 +0700

    Updated Webpack: Added Hot Module Replacement (HMR) support

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js', // Đường dẫn đến tệp entry của bạn
+    entry: './src/index.tsx', // Đường dẫn đến tệp entry của bạn
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'), // Thư mục đầu ra
+        publicPath: '/', // Đảm bảo rằng publicPath được thiết lập
     },
     module: {
         rules: [
@@ -19,11 +21,22 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.css$/, // Thêm quy tắc để xử lý CSS
+                use: ['style-loader', 'css-loader'],
+            },
             // Có thể thêm các quy tắc khác ở đây
         ],
     },
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'], // Các định dạng tệp mà Webpack sẽ nhận diện
     },
-    // Các tùy chọn khác...
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'), // Thư mục phục vụ
+        hot: true, // Bật Hot Module Replacement
+        historyApiFallback: true, // Hỗ trợ cho React Router
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(), // Bật HMR
+    ],
 };
