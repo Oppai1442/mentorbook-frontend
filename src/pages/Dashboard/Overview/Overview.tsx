41702fed8ebe4451d8baec09b17fb400d1dc86cc
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-31 21:29:16 +0700

    Created Overview page: Added child page under Dashboard

---
+++ src/pages/Dashboard/Overview/Overview.tsx
@@ -0,0 +1,11 @@
+// Overview.tsx
+import React from 'react';
+
+const Overview: React.FC = () => (
+  <div>
+    <h2>Overview</h2>
+    <p>Welcome to the Overview section of your dashboard!</p>
+  </div>
+);
+
+export default Overview;
