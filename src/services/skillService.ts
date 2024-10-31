93234eb1189424ad90389e7a2056b013c2481856
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-31 21:34:50 +0700

    Updated SkillService: Changed getAllSkills method to use await getData<Skill[]> instead of await getData<SkillResponse[]>

--- src/services/skillService.ts
+++ src/services/skillService.ts
@@ -1,9 +1,9 @@
+import { Skill } from "../types/Model";
 import { getData } from "./apiService";
-import { SkillResponse } from "../types/Response";
 
 export const getAllSkills = async () => {
   try {
-    const response = await getData<SkillResponse[]>(`/api/skill/get-all-skills`);
+    const response = await getData<Skill[]>(`/api/skill/get-all-skills`);
     
     return response.data
   } catch (error) {
