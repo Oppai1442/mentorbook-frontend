4e95738cb07f91787fc2692856a620de5bd2e17a
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-11-03 00:50:05 +0700

    Updated index.ts: Exported Users page and Transactions page

--- src/pages/Dashboard/index.ts
+++ src/pages/Dashboard/index.ts
@@ -1,3 +1,5 @@
 export { default as Dashboard } from './Dashboard';
 export { default as Overview} from './Overview/Overview'
+export { default as Users} from './Users/Users'
+export { default as Transaction} from './Transaction/Transaction'
 export { default as Settings} from './Settings/Settings'
